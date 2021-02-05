a51e09efefe59f119986eeb35f2223aa513024fe
Key Admin <mdepeiza@keyconceptskb.com>
2021-02-05 12:46:13 -0500

    Added main code.

--- MyBinarySearchTree/Program.cs
+++ MyBinarySearchTree/Program.cs
@@ -10,6 +10,15 @@ namespace MyBinarySearchTree
     {
         static void Main(string[] args)
         {
+            BinarySearchTree bst = new BinarySearchTree();
+            bst.Add(new Node(3));
+            bst.Add(new Node(5));
+            bst.Add(new Node(1));
+            bst.Add(new Node(4));
+            bst.Add(new Node(7));
+
+            bst.Search(new Node(7));
+            bst.Search(new Node(9));
         }
     }
 }
