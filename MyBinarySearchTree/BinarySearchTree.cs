bbd352c0b0628876d497333285c20d70413f0436
Key Admin <mdepeiza@keyconceptskb.com>
2021-02-05 12:18:18 -0500

    Implemented Add method in BinarySearchTree class.

--- MyBinarySearchTree/BinarySearchTree.cs
+++ MyBinarySearchTree/BinarySearchTree.cs
@@ -8,5 +8,49 @@ namespace MyBinarySearchTree
 {
     class BinarySearchTree
     {
+        public Node rootNode;
+
+        public void Add(Node nodeToAdd)
+        {
+            if(rootNode == null)
+            {
+                rootNode = nodeToAdd;
+            }
+            else
+            {
+                Node focusNode = rootNode;
+                while (true)
+                {
+                    if (nodeToAdd.data.CompareTo(focusNode) < 0)
+                    {
+                        if (focusNode.leftNode == null)
+                        {
+                            focusNode.leftNode = nodeToAdd;
+                            break;
+                        }
+                        else
+                        {
+                            focusNode = focusNode.leftNode;
+                        }
+                    }
+                    else if(nodeToAdd.data.CompareTo(focusNode) > 0)
+                    {
+                        if (focusNode.rightNode == null)
+                        {
+                            focusNode.rightNode = nodeToAdd;
+                            break;
+                        }
+                        else
+                        {
+                            focusNode = focusNode.rightNode;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
